6bf8e92e963589c2d706c5b6e558db03200ccece
m.rapacz <m.rapacz@getprintbox.com>
2024-02-23 20:16:00 +0100

    add cookieyes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  app: {
+    head: {
+      script: [
+        {
+          src: "https://cdn-cookieyes.com/client_data/fcf743ce2c6c407518757022/script.js",
+          type: "text/javascript",
+          id: "cookieyes",
+        },
+      ],
+    },
+  },
+});
