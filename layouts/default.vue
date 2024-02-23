213712b5f68c92927383ced4d515a6d5b3cf6fb0
m.rapacz <m.rapacz@getprintbox.com>
2024-02-23 20:39:07 +0100

    c

---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav :class="$style.nav">
+    <NuxtLink to="/">Homepage</NuxtLink>
+    <NuxtLink to="/cart">Cart</NuxtLink>
+    <NuxtLink to="/about-us">About us</NuxtLink>
+    <NuxtLink to="/contact-us">Contact us</NuxtLink>
+  </nav>
+  <div :class="$style.page">
+    <NuxtPage />
+  </div>
+</template>
+
+<style lang="scss" module>
+.nav {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 2rem;
+}
+.page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 80vh;
+}
+</style>
